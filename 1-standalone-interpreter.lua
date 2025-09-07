fe0342cd4cefe8d084c9c5c7511a28cbaba41868
DallasSteele <harleenq658@gmail.com>
2025-09-07 15:23:32 +0700

    forgot the comments

---
+++ 1-standalone-interpreter.lua
@@ -0,0 +1,9 @@
+---no need to dive deep here
+---it's quite useful if we use another lua interpreter from what i have gathered
+---https://www.lua.org/pil/1.4.html
+---
+---although it let's you run prompts straight to the terminal
+---
+
+
+
