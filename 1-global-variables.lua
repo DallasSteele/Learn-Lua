31dbca8c552030c50481d4f81589d0d28cdd368b
DallasSteele <harleenq658@gmail.com>
2025-09-07 15:16:18 +0700

    first chapter done

---
+++ 1-global-variables.lua
@@ -0,0 +1,8 @@
+print (b) --> nil
+b = 10
+print(10) --> 10
+
+print ("brb")
+
+b = nil
+print (b) --> nil
---
+++ 1-lexical-conventions.lua
@@ -0,0 +1,10 @@
+--[[
+print (10) 
+--]]  --no action according to documentations
+
+---[[
+---print (10)
+---
+--- --]] -->10
+--- 
+print("it's in full comments, better check out if one need a comment helper")
